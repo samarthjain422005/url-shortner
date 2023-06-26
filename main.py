76f9255d1ad5e928f11852e2b2de4f60ade4d75d
SAMARTH JAIN <samarthj422005@gmail.com>
2023-06-26 18:11:38 +0530

    feat: made changes to backend

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask , render_template
+from flask import Flask , render_template ,request
 from inputform import InputForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "#sammyboy/url-shortner"
 
-@app.route("/")
+shortened_urls=[{"destination_url": "http://google.com ,"id":"1234" }]
+
+@app.route("/",methods=["GET", "POST"])
 def home():
     form=InputForm()
+    if request.method=="POST":
+            if form.validate_on_submit(): 
+                 print(form.url_input.data)
+            else:
+                 print("failed")
     return render_template("index.html",form=form)
 
 if __name__ == "__main__":
