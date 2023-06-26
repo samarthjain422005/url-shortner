04d425fa087b5a64689ca71d387c710c269cd4b9
SAMARTH JAIN <samarthj422005@gmail.com>
2023-06-26 23:59:06 +0530

    feat: minor changes

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
-from flask import Flask , render_template ,request,flash,redirect,abort
-from inputform import InputForm
+from flask import Flask, render_template, request, flash, redirect, abort
+from InputForm import InputForm
 import secrets
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "#sammyboy/url-shortner"
+app.config['SECRET_KEY'] = "abc"
 
-shortened_urls=[{"destination_url": "http://google.com ,"id":"1234" }]
+shortened_urls = []
 
-@app.route("/",methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    form=InputForm()
-    if request.method=="POST":
-            if form.validate_on_submit(): 
-                 destination_url=form.url_input.data
-                 id=secrets.token_urlsafe(8)
-                 shortened_urls.append({"destination_url":destination_url,"id":id})
-                 form.url_input.data=""
-                 flash(f"Success!! your shortned url is: {request.base_url+id}",category="success message")
-            else:
-                 flash("Invalid url" , category="error message")
-    return render_template("index.html",form=form)
+    form = InputForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            id = secrets.token_urlsafe(8)
+            shortened_url = request.base_url + id
+            shortened_urls.append({"destination_url": form.url.data, "id": id})
+            flash(f"Shortened URL: {shortened_url}", "success message")
+            form.url.data=''
+        else:
+            flash("Invalid URL!", "error message")
+    return render_template("index.html", form=form)
 
 @app.route("/<id>")
-def redirect_user(id):
-    for url in shortened_urls:
-        if url["id"]==id:
-            return redirect(url["destination_url"])
+def shortened(id):
+    for shortened_url in shortened_urls:
+        if shortened_url["id"] == id:
+            return redirect(shortened_url["destination_url"])
     return abort(404)
 
-if __name__=="__main__":
-
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
-
+    app.run(debug=True)
